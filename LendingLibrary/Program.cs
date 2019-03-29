23593a9b7fcffcffc31c8162de7c28657ed82f03
Jason Burns <jasonb315@protonmail.com>
2019-03-28 22:07:45 -0700

    cli display

--- LendingLibrary/Program.cs
+++ LendingLibrary/Program.cs
@@ -8,9 +8,33 @@ namespace LendingLibrary
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            string appName = "Library Lender";
+            string appVersion = "1.0.0";
+            string appAuthor = "Jason Burns";
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
 
+            try
+            {
+                Borrow();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("Exiting Program");
+            }
+        }
+
+        public static void Borrow()
+        {
             Library<Book> library = new Library<Book>();
-
             library.Add(new Book("Harry Potter", new Author("J.K. Rawling"), Book.Genres.Fantasy));
             library.Add(new Book("1989", new Author("George Orwell"), Book.Genres.Romance));
             library.Add(new Book("Brave New World", new Author("Aldous Huxley"), Book.Genres.Psychology));
@@ -22,36 +46,58 @@ namespace LendingLibrary
             library.Add(new Book("Wubbalubbadubdub", new Author("Rick"), Book.Genres.theology));
             library.Add(new Book("The God Molecule", new Author("Dr. Strauss"), Book.Genres.theology));
 
-
-            foreach(Book book in library)
+            // readout books
+            Console.WriteLine("BOOKS IN LIBRARY");
+            foreach (Book book in library)
             {
                 Console.WriteLine($"{book.writer.Name} wrote {book.Title}");
             }
 
+            // two readers
             List<Book> readerA = new List<Book>();
             List<Book> readerB = new List<Book>();
 
-            Console.WriteLine(".....................");
-
+            // distribute books to readers
             foreach (Book book in library)
             {
                 if (book.genre == Book.Genres.Fantasy)
                 {
                     library.Remove(book);
                     readerA.Add(book);
-                    Console.WriteLine("ding");
                 }
-
+            }
+            foreach (Book book in library)
+            {
+                if (book.genre == Book.Genres.Horror)
+                {
+                    library.Remove(book);
+                    readerB.Add(book);
+                }
             }
 
+            // print the books that each reader checked out
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine();
+            Console.WriteLine("READER A CHECKED OUT");
             foreach (Book book in readerA)
             {
                 Console.WriteLine($"{book.writer.Name} wrote {book.Title}");
             }
 
-            Console.WriteLine(".....................");
+
+            Console.WriteLine();
+            Console.WriteLine("READER B CHECKED OUT");
+            foreach (Book book in readerB)
+            {
+                Console.WriteLine($"{book.writer.Name} wrote {book.Title}");
+            }
 
 
+            // print out updated library contents
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.WriteLine("BOOKS IN LIBRARY");
             foreach (Book book in library)
             {
                 Console.WriteLine($"{book.writer.Name} wrote {book.Title}");
