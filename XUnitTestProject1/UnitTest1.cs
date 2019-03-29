ab16c2b3986c4a3ae30c936e9b3b22a1880d259e
Jason Burns <jasonb315@protonmail.com>
2019-03-28 22:15:33 -0700

    todo: tests, summary strings

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -12,3 +12,11 @@ namespace XUnitTestProject1
         }
     }
 }
+
+//Add a Book to your Library that exists
+//Remove a book from your library
+//Cannot remove a book from the library that doesn’t exist.
+//Getter/Setters of your properties from your Book class
+//Getter/Setters of your properties from your Author class.
+//Accurate count of books within the library
+//One edge case of your choice
